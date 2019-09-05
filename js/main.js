86b46e60be97111b06b44dc3f57842b08f8706df
Kevin Arnall <kevinjarnall@gmail.com>
2019-09-05 14:36:25 -0500

    logo

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // Check for click events on the navbar burger icon
-    $(".navbar-burger").click(function() {
+    $(".navbar-burger").click(function () {
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
     });
+
+    setInterval(function () {
+        $('#cursor').animate({
+            opacity: 1
+        }, 400).animate({
+            opacity: 0
+        }, 400)
+    }, 100);
 });
